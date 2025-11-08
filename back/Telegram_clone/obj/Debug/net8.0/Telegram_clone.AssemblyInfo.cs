ffd0efe63f3d9484bb4dac5edcce03d8bf980e36
Dmytro Potapchuk <40177161+potapchukdmytro@users.noreply.github.com>
2025-11-08 10:06:44 +0200

    fix front prject arch

--- back/Telegram_clone/obj/Debug/net8.0/Telegram_clone.AssemblyInfo.cs
+++ back/Telegram_clone/obj/Debug/net8.0/Telegram_clone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Telegram_clone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7d42c4ded1ea9eb395daa9e6cab91f4b32b813d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Telegram_clone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Telegram_clone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
